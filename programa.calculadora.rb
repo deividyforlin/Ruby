64182338a76fb3a12e6a1462a5be164fd4c2a668
deividyforlin <deividy.forlin@gmail.com>
2025-04-22 09:24:40 -0300

    feita correção da troca de numeros1,2 ect para operação +,-,*,/

---
+++ programa.calculadora.rb
@@ -0,0 +1,21 @@
+puts "Digite o primeiro número:"
+num1 = gets.chomp.to_f  
+puts "Digite o segundo número:"
+num2 = gets.chomp.to_f  
+puts "Digite a operação desejada (+, -, *, /):"
+operacao = gets.chomp  # Agora espera um símbolo
+
+r = case operacao
+when "+"
+  num1 + num2
+when "-"
+  num1 - num2
+when "*"
+  num1 * num2
+when "/"
+  num1 / num2
+else
+  "Operação inválida!"
+end
+
+puts "O resultado é: #{r}"
